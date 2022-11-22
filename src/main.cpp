bab2b87e41e2a9811d96b7bf81fc2a4f27375644
ilintr.24 <tima.ilin06@yandex.ru>
2022-11-22 12:52:22 +0300

    imgui connected

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,53 @@
-#include <SFML/Graphics.hpp>
+#include <SFML/Graphics/RenderWindow.hpp>
+#include <SFML/System/Clock.hpp>
+#include <SFML/Window/Event.hpp>
 
+#include <imgui-SFML.h>
+#include <imgui.h>
+
+// главный метод
 int main() {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    // создаём окно для рисования
+    sf::RenderWindow window(sf::VideoMode(1280, 720), "ImGui + SFML = <3");
+    // задаём частоту перерисовки окна
+    window.setFramerateLimit(60);
+    // инициализация imgui+sfml
+    ImGui::SFML::Init(window);
 
+    // переменная таймера
+    sf::Clock deltaClock;
+    // пока окно открыто, запускаем бесконечный цикл
     while (window.isOpen()) {
+        // создаём событие sfml
         sf::Event event;
+        // пока окно принимает события
         while (window.pollEvent(event)) {
-            if (event.type == sf::Event::Closed)
+            // отправляем события на обработку sfml
+            ImGui::SFML::ProcessEvent(event);
+
+            // если событие - это закрытие окна
+            if (event.type == sf::Event::Closed) {
+                // закрываем окно
                 window.close();
+            }
         }
 
+        // запускаем обновление окна по таймеру с заданной частотой
+        ImGui::SFML::Update(window, deltaClock.restart());
+
+        // рисуем демонстрационное окно
+        ImGui::ShowDemoWindow();
+
+        // очищаем окно
         window.clear();
-        window.draw(shape);
+        // рисуем по окну средствами imgui+sfml
+        ImGui::SFML::Render(window);
+        // отображаем изменения на окне
         window.display();
     }
+
+    // завершаем работу imgui+sfml
+    ImGui::SFML::Shutdown();
 
     return 0;
 }
